8a0fe955c69740dfff5c4cb41f268f7a3b166e77
Otabek Toshkanov <toshkanov.otabek@gmail.com>
2024-06-01 19:43:33 +0500

    first commit

---
+++ src/test/java/io/github/otabektoshkanov/routing_solver/RoutingSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.otabektoshkanov.routing_solver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
